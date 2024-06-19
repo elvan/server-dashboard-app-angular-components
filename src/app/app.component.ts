bdffed6e3fa0b12edd071f94c2eb880eddc7cc8e
Elvan Hidayat <elvanhd@gmail.com>
2024-06-20 00:41:32 +0700

    Refactor dashboard components for modularity
    - Replace header section in app.component.html with <app-header />
    - Replace server status section with <app-server-status />
    - Replace traffic section with <app-traffic />
    - Replace support tickets section with <app-tickets />
    - Update app.component.ts to import new components
    - Create ServerStatusComponent with server-status content
    - Create TicketsComponent with support tickets content
    - Create TrafficComponent with traffic content
    - Create HeaderComponent with header content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,19 @@
 import { Component } from '@angular/core';
 
+import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
+import { TicketsComponent } from './dashboard/tickets/tickets.component';
+import { TrafficComponent } from './dashboard/traffic/traffic.component';
+import { HeaderComponent } from './header/header.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [
+    HeaderComponent,
+    ServerStatusComponent,
+    TrafficComponent,
+    TicketsComponent,
+  ],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  dummyTrafficData = [
-    {
-      id: 'd1',
-      value: 433,
-    },
-    {
-      id: 'd2',
-      value: 260,
-    },
-    {
-      id: 'd3',
-      value: 290,
-    },
-    {
-      id: 'd4',
-      value: 410,
-    },
-    {
-      id: 'd5',
-      value: 397,
-    },
-    {
-      id: 'd6',
-      value: 488,
-    },
-    {
-      id: 'd47',
-      value: 589,
-    },
-  ];
-  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
-  currentStatus = 'online';
-}
+export class AppComponent {}
