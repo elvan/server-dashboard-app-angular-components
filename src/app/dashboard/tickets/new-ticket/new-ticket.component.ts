c9fd3c90dc2567c10f14b0d7a98787ea4a0285f2
Elvan Hidayat <elvanhd@gmail.com>
2024-06-20 21:52:23 +0700

    Refactor button component to support content projection
    - Update NewTicketComponent to use ButtonComponent
    - Modify button HTML in new-ticket.component.html
    - Import ButtonComponent in new-ticket.component.ts
    - Update button in HeaderComponent to use ButtonComponent
    - Modify button HTML in header.component.html
    - Refactor ButtonComponent template for content projection

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import { ButtonComponent } from '../../../shared/button/button.component';
+
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
