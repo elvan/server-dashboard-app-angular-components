17ba495e6f40125791ba5b37bd8284800c93765d
Elvan Hidayat <elvanhd@gmail.com>
2024-06-20 23:05:06 +0700

    Update server status component with conditional classes
    - Add conditional class binding in server-status.component.html
    - Modify currentStatus to 'offline' in server-status.component.ts

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './server-status.component.css',
 })
 export class ServerStatusComponent {
-  currentStatus = 'online';
+  currentStatus = 'offline';
 }
