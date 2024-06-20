9ef2888fd8737b23e271f7503e300b91ec626655
Elvan Hidayat <elvanhd@gmail.com>
2024-06-20 21:40:10 +0700

    Add new ticket form and refactor button component
    - Create NewTicketComponent for adding new support tickets
    - Add form layout in new-ticket.component.html
    - Add basic CSS for new-ticket component
    - Update TicketsComponent to include NewTicketComponent
    - Create TicketComponent for individual ticket display
    - Refactor button in header to use ButtonComponent
    - Create ButtonComponent for reusable button design
    - Add CSS for tickets section and new ticket form in styles.css

---
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket',
+  standalone: true,
+  imports: [],
+  templateUrl: './ticket.component.html',
+  styleUrl: './ticket.component.css',
+})
+export class TicketComponent {}
--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { NewTicketComponent } from './new-ticket/new-ticket.component';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
+  imports: [NewTicketComponent],
 })
 export class TicketsComponent {}
